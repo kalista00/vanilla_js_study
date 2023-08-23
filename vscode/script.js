a79c93e3d4846e751e4563f4996b1cde55b24993
김창희 <122184547+kalista00@users.noreply.github.com>
2023-08-23 19:24:16 +0900

    Add files via upload

--- vscode/script.js
+++ vscode/script.js
@@ -1,2 +1,24 @@
-const a = 2+2;
+const a = 5;
+let b = 6;
 alert(a);
+console.log(a,"2");
+console.log(a*b);
+const amIFat = null;
+let something;
+console.log(something);
+console.log(amIFat);
+
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+//array
+const dayOfweek = [wed, thu, fri, true, "hello"];
+console.log(dayOfweek);
+console.log(dayOfweek[3]);
+dayOfweek.push("ee");
+console.log(dayOfweek[5]);
+const player = {
+    name : "창희",
+    points: 10,
+    fat : true,
+}
